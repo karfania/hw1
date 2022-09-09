5d6b5b932c82f8b7069fd27ee1665b6c003ef365
Kourosh Arfania <arfania@usc.edu>
2022-09-08 23:43:06 -0700

    Check in changes for problem 2

--- ulliststr.cpp
+++ ulliststr.cpp
@@ -25,6 +25,268 @@ size_t ULListStr::size() const
 }
 
 // WRITE YOUR CODE HERE
+void ULListStr::push_back(const std::string& val)
+{
+  // If the LL is empty, create a new Node
+  if (empty())
+  {
+    // LL of 1 node, head and tail point to same node
+    Item* node = new Item();
+    head_ = node;
+    tail_ = node;
+
+    // Prev and next of Item are NULL
+    node -> prev = NULL;
+    node -> next = NULL;
+
+    // First and last elements are the same
+    // Therefore, first points to index 0
+    // Therefore, Last points to index 1
+    node -> first = 0;
+    node -> last = node -> first + 1;
+
+    // Set value at first index in node to passed-in val
+    node -> val[node -> first] = val;
+  }
+
+  // If there is space to push back within the node
+  // Referencing values of tail since it is at the end of the node
+  else if (tail_ -> last < ARRSIZE)
+  {
+    // Push last up one
+    tail_ -> last = tail_ -> last + 1;
+
+    // Add it to values at the end of the contiguous array
+    tail_ -> val[(tail_ -> last) - 1] = val;
+  }
+
+  // If there the list isn't empty but there's no space
+  // inside current node, create new node after current node
+  // and update tail and next/prev
+  else
+  {
+    // LL of 1 node, update tail
+    Item* temp = tail_;
+    Item* node = new Item();
+
+    // temp's next (aka tail_'s next) is now the new node.
+    // temp -> prev is already accurate
+    temp -> next = node;
+
+    // The node before the new node is temp (aka tail_)
+    node -> prev = temp;
+
+    // First and last elements are the same
+    // Therefore, first points to index 0
+    // Therefore, Last points to index 1
+    node -> first = 0;
+    node -> last = node -> first + 1;
+
+    // Update the values of the node
+    node -> val[node -> first] = val;
+
+    // Finally, set tail to the new node
+    tail_ = node;
+  }
+
+  // Update size regardless of if statement
+  size_ = size_ + 1;
+}
+
+void ULListStr::push_front(const std::string& val)
+{
+  // If the LL is empty, create a new Node
+  if (empty())
+  {
+    // LL of 1 node, head and tail point to same node
+    Item* node = new Item();
+    head_ = node;
+    tail_ = node;
+
+    // Prev and next of Item are NULL
+    node -> prev = NULL;
+    node -> next = NULL;
+
+    // First and last elements are the same
+    // Therefore, first points to index ARRSIZE-1
+    // Therefore, last points to index + 1 aka ARRSIZE
+    node -> first = ARRSIZE - 1;
+    node -> last = node -> first + 1;
+
+    // Set value at first index in node to passed-in val
+    node -> val[node -> first] = val;
+  }
+
+  // If there is space to push front within the node
+  // Referencing values of head since it is at the start of the node
+  else if (head_ -> first > 0)
+  {
+    // Push first down one
+    head_ -> first = head_ -> first - 1;
+
+    // Add it to values at new first index
+    head_ -> val[head_ -> first] = val;
+  }
+
+  // If there the list isn't empty but there's no space
+  // inside current node, create new node before current
+  // node and update head and next/prev
+  else
+  {
+    // LL of 1 node, update tail
+    Item* temp = head_;
+    Item* node = new Item();
+
+    // temp's prev (aka head_'s prev) is now the new node.
+    // temp -> next is already accurate
+    temp -> prev = node;
+
+    // The node after the new node is temp (aka head_)
+    node -> next = temp;
+
+    // First and last elements are the same
+    // Therefore, first points to index ARRSIZE-1
+    // Therefore, Last points to ARRSIZE
+    node -> first = ARRSIZE - 1;
+    node -> last = node -> first + 1;
+
+    // Update the values of the node
+    node -> val[node -> first] = val;
+
+    // Finally, set head to the new node
+    head_ = node;
+  }
+
+  // Update size regardless of if statement
+  size_ = size_ + 1;
+}
+
+void ULListStr::pop_back()
+{
+  // If list is empty, just return nothing
+  if (empty())
+  {
+    return;
+  }
+
+  // If the list only have 1 element, need to set 
+  // head and tail to NULL and delete the node
+  if (size_ == 1)
+  {
+    delete tail_;
+    head_ = NULL;
+    tail_ = NULL;
+  }
+
+  // If the last pointer of the node tail_ points to
+  // is at index 1, this means one one element
+  // exists at index 0 thus removal empties the node
+  else if (tail_ -> last == 1)
+  {
+    // Change tail to be one node prior
+    tail_ = tail_ -> prev;
+
+    // Delete the node after tail
+    delete tail_ -> next;
+    tail_ -> next = NULL;
+  }
+
+  // Otherwise, simply remove the element within node 
+  // by decrementing last pointer
+  else
+  {
+    tail_ -> last = (tail_ -> last) -1;
+  }
+
+  // Update size regardless of if statement
+  size_ = size_ - 1;
+  
+}
+
+void ULListStr::pop_front()
+{
+  // If list is empty, just return nothing
+  if (empty())
+  {
+    return;
+  }
+
+  // If the list only have 1 element, need to set 
+  // head and tail to NULL and delete the node
+  if (size_ == 1)
+  {
+    delete head_;
+    head_ = NULL;
+    tail_ = NULL;
+  }
+
+  // If the first pointer of the node head_ points to
+  // is at index ARRSIZE-1, this means one one element
+  // exists at the end of the node's array thus removal empties the node
+  else if (head_ -> first == ARRSIZE-1)
+  {
+    // Change head_ to be one node after
+    head_ = head_ -> next;
+
+    // Delete the node before head_
+    delete head_ -> prev;
+    head_ -> prev = NULL;
+  }
+
+  // Otherwise, simply remove the element within node 
+  // by incrementing first pointer
+  else
+  {
+    head_ -> first = (head_ -> first) + 1;
+  }
+
+  // Update size regardless of if statement
+  size_ = size_ - 1;
+  
+}
+
+// Returning the value located in the last element of tail's array
+std::string const & ULListStr::back() const
+{
+  return tail_ -> val[tail_ -> last - 1];
+}
+
+// Returning the value located in the first element of head's array
+std::string const & ULListStr::front() const
+{
+  return head_ -> val[head_ -> first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const
+{
+  // Ensuring loc is within bounds of array
+  if (loc >= 0 && loc < size_)
+  {
+    // Tracking process of getting to loc
+    size_t index = 0;
+    Item* currNode = head_;
+    size_t element = head_ -> first;
+    while (index < loc)
+    {
+      element++;
+      // If at the end of current node, move to next
+      // and reset indexing of node
+      if (element == currNode -> last)
+      {
+        currNode = currNode -> next;
+        element = 0;
+      }
+      index++;
+    }
+    return &currNode -> val[element];
+  }
+
+  // Out of bounds 
+  else
+  {
+    return NULL;
+  }
+}
 
 void ULListStr::set(size_t loc, const std::string& val)
 {
--- ulliststr_test.cpp
+++ ulliststr_test.cpp
@@ -1,11 +1,98 @@
 /* Write your test code for the ULListStr in this file */
 
 #include <iostream>
+#include "ulliststr.h"
+#include <string>
 using namespace std;
 
 
 int main(int argc, char* argv[])
 {
 
-  return 0;
+  // CODIO EXAMPLE
+  // Tests push_back() on empty list & populated node
+  // Tests push_front() on populated node
+  // Tests getValAtLoc(), front(), and back()
+  cout << "INITIALIZING LIST WITH 8 7 9" << endl;
+  ULListStr list;
+  list.push_back("7");
+  list.push_front("8");
+  list.push_back("9");
+  cout << "All elements: " << list.get(0) << " " << list.get(1) << " " << list.get(2) << endl; // should print 8 7 9
+  cout << "Front of the list: " << list.front() << endl; // should print 8
+  cout << "Back of the list: " << list.back() << endl; // should print 9
+  cout << "Current size of linked list: " << list.size() << endl; // should be 3
+  cout << endl;
+
+
+  // EMPTYING LL
+  // Tests pop_front() on populated node
+  // Tests pop_back() on populated node
+  cout << "POPPED FRONT " << endl;
+  list.pop_front(); // removing 8 --> new front = 7
+  cout << "All elements: " << list.get(0) << " " << list.get(1) << endl; // should print 7 9
+  cout << "Current front: " << list.front() << endl; // should be 7
+  cout << "Current back: " << list.back() << endl; // should be 9
+  cout << "Current size of linked list: " << list.size() << endl; // should be 2
+  cout << endl;
+
+  cout << "POPPED BACK" << endl;
+  list.pop_back(); // removing 9 --> new back and front = 7
+  cout << "All elements: " << list.get(0) << endl; // should print 7 
+  cout << "Current front: " << list.front() << endl; // should be 7
+  cout << "Current back: " << list.back() << endl; // should be 7
+  cout << "Current size of linked list: " << list.size() << endl; // should be 1
+  cout << "The list is empty: " << boolalpha << list.empty() << endl; // should be false
+  cout << endl;
+  
+  cout << "POPPED FRONT -> EMPTY LIST" << endl;
+  list.pop_front();
+  cout << "The list is empty: " << boolalpha << list.empty() << endl; // should be true
+  cout << endl;
+
+  // ADDING TO LINKED LIST -> FILLING ENTIRELY
+  // Tests push_front() on empty list & populated node
+  cout << "PUSHED FRONT 10 ELEMENTS TO FILL NODE" << endl;
+  for (int i = 0; i < 10; i++)
+  {
+    string input = to_string(i);
+    list.push_front(input);
+    cout << "Added " << input << " to list" << endl;
+  }
+
+  cout << "Current size of linked list: " << list.size() << endl; // should be 10
+  cout << "All elements: " << list.get(0) << " " << list.get(1) << " " 
+  << list.get(2) << " " << list.get(3) << " " << list.get(4) << " " << 
+  list.get(5) << " " << list.get(6) << " " << list.get(7) << " " << list.get(8) <<
+  " " << list.get(9) << endl; // should print 9 8 7 6 5 4 3 2 1 0
+  cout << endl;
+
+  // POPULATING FULL NODE
+  // Tests push_front() and push_back() on full node
+  list.push_back("-1");
+  list.push_front("10");
+
+  cout << "Current size of linked list: " << list.size() << endl; // should be 12
+  cout << "All elements: " << list.get(0) << " " << list.get(1) << " " 
+  << list.get(2) << " " << list.get(3) << " " << list.get(4) << " " << 
+  list.get(5) << " " << list.get(6) << " " << list.get(7) << " " << list.get(8) <<
+  " " << list.get(9) << " " << list.get(10) << " " << list.get(11) << endl; // should print 10 9 8 7 6 5 4 3 2 1 0 -1
+
+  cout << "Current front: " << list.front() << endl; // should be 10
+  cout << "Current back: " << list.back() << endl; // should be -1
+
+  // TESTING REMAINING FUNCTIONS
+  cout << "CHANGED VALUE 8 AT LOC 2 FROM 8 TO 88" << endl;
+  list.set(2, "88");
+  cout << "Value at loc 2: " << list.get(2) << endl; // should be 88
+  cout << "All elements: " << list.get(0) << " " << list.get(1) << " " 
+  << list.get(2) << " " << list.get(3) << " " << list.get(4) << " " << 
+  list.get(5) << " " << list.get(6) << " " << list.get(7) << " " << list.get(8) <<
+  " " << list.get(9) << " " << list.get(10) << " " << list.get(11) << endl; // should print 10 9 88 7 6 5 4 3 2 1 0 -1
+  cout << endl;
+
+  cout << "CLEARED THE LINK LIST" << endl;
+  list.clear();
+  cout << "The list is empty: " << boolalpha << list.empty() << endl; // should be true
+  
 }
